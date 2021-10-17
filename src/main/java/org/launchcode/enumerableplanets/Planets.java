c6dd97da43844831e39f48938234fc2eb78d34bd
swankidelic <swankidelic@gmail.com>
2021-10-17 16:37:47 -0500

    exercise 14.3 step 6 complete

---
+++ src/main/java/org/launchcode/enumerableplanets/Planets.java
@@ -0,0 +1,13 @@
+package org.launchcode.enumerableplanets;
+
+public enum Planets {
+    MERCURY("Mercury"),
+    VENUS("Venus"),
+    EARTH("EARTH"),
+    MARS("Mars"),
+    JUPITER("Jupiter"),
+    SATURN("Saturn"),
+    URANUS("Uranus"),
+    NEPTUNE("Neptune");
+
+}
